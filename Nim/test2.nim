f25eda425323a2fc98441b47edb363a21c926299
merhab <merhab@gmail.com>
2023-05-08 19:33:02 +0200

    for windows os

--- Nim/test2.nim
+++ Nim/test2.nim
@@ -5,7 +5,8 @@ import QPushButton,QWidget,QAbstractButton
     
 var app = newQApplication()
 var btn = newQPushButton(nil)
-proc onClick()=
+proc onClick(){.cdecl.}=
+    echo "clicked"
     btn.setText("clicked")
 btn.show
 btn.onClickedConnect(onClick)
